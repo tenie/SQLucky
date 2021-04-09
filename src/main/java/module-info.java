34e74810a94fc8d4e68763eddece926777ad636a
tenie <tenie@tenie.net>
2021-04-09 14:11:24 +0800

    up

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,21 +10,26 @@ open module SQLucky {
 	requires java.scripting;
 	requires java.xml;
     requires java.sql; 
+    
 	requires javafx.base;
 	requires javafx.graphics;
     requires javafx.controls;
-    requires javafx.fxml;
-	requires org.apache.commons.io;
+    requires javafx.fxml;  
+    // mods
+	requires com.jfoenix;  
+	requires org.controlsfx.controls;
 	requires org.apache.logging.log4j;	
+
+	
+	// non-mods
+	requires org.apache.commons.io;
+	requires org.fxmisc.richtext; 
 //	requires com.h2database;	
 	requires h2;
-	requires org.fxmisc.richtext;
 	requires reactfx;
 	requires flowless;
-	requires com.jfoenix;
-	requires org.controlsfx.controls;
 	requires sql.formatter;
 	requires slf4j.api;
 	exports net.tenie.fx.controller  to    javafx.fxml;
-    exports net.tenie.fx.main to    javafx.graphics;
+    exports net.tenie.fx.main        to    javafx.graphics;
 }
