b065133e0ff31b0362ddfedda697b3deff4fee2f
amd64G <tenie@tenie.net>
2023-03-09 23:34:17 +0800

    update

--- plugin/WorkDataBackup/src/main/java/net/tenie/plugin/backup/component/SqluckyBackup.java
+++ plugin/WorkDataBackup/src/main/java/net/tenie/plugin/backup/component/SqluckyBackup.java
@@ -8,7 +8,8 @@ public class SqluckyBackup {
 	private Long userId;
 	private String backupName;
 	private String filePath;
-	private Integer type;
+	private String typeInfo;
+	private String usePrivateKey;
 	private Date createdAt;
 	private Date updatedAt;
 	
@@ -44,24 +45,33 @@ public class SqluckyBackup {
 	public void setUpdatedAt(Date updatedAt) {
 		this.updatedAt = updatedAt;
 	}
-	public Integer getType() {
-		return type;
-	}
-	public void setType(Integer type) {
-		this.type = type;
-	}
+
 	public Long getUserId() {
 		return userId;
 	}
 	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
+	
+	public String getTypeInfo() {
+		return typeInfo;
+	}
+	public void setTypeInfo(String typeInfo) {
+		this.typeInfo = typeInfo;
+	}
+	public String getUsePrivateKey() {
+		return usePrivateKey;
+	}
+	public void setUsePrivateKey(String usePrivateKey) {
+		this.usePrivateKey = usePrivateKey;
+	}
 	@Override
 	public String toString() {
 		return "SqluckyBackup [id=" + id + ", userId=" + userId + ", backupName=" + backupName + ", filePath="
-				+ filePath + ", type=" + type + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
+				+ filePath + ", typeInfo=" + typeInfo + ", usePrivateKey=" + usePrivateKey + ", createdAt=" + createdAt
+				+ ", updatedAt=" + updatedAt + "]";
 	}
- 
+	 
 	  
 	
 	
