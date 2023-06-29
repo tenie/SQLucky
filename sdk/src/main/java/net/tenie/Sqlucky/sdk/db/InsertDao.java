8ec945994feb41381fb4c23ae28dac38b48027c4
tenie <snoopy32@qq.com>
2023-06-29 23:30:53 +0800

    update

--- sdk/src/main/java/net/tenie/Sqlucky/sdk/db/InsertDao.java
+++ sdk/src/main/java/net/tenie/Sqlucky/sdk/db/InsertDao.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -70,15 +71,33 @@ public class InsertDao {
 				String columnTypeName = cellPo.getField().getColumnTypeName().get();
 				logger.info("javatype = " + javatype + " | " + columnTypeName);
 				if (CommonUtility.isDateAndDateTime(javatype)) {
-					Date dv = StrUtils.StrToDate_L(val);
-					pstmt.setObject(idx, dv);
+					if (StrUtils.isNullOrEmpty(val.trim())) {
+						pstmt.setObject(idx, null);
+						insertLog += " | null";
+					} else {
+						Date dv = StrUtils.StrToDate_L(val);
+						pstmt.setObject(idx, dv);
+						insertLog += " | " + val;
+					}
+
 //				if (CommonUtility.isDateTime(javatype)) {
 //					Date dv = DateUtils.StrToDate(val, ConfigVal.dateFormateL);
 //					Timestamp ts = new Timestamp(dv.getTime());
 //					pstmt.setTimestamp(idx, ts);
-					insertLog += " | " + dv;
-				} else if (CommonUtility.isNum(javatype) && StrUtils.isNullOrEmpty(val.trim())) {
-					pstmt.setObject(idx, null);
+
+					// 是数字又是空字符串，设置为null
+				} else if (CommonUtility.isNum(javatype)) {
+					val = val.trim();
+					if (StrUtils.isNullOrEmpty(val)) { // 空字符串， 设置null
+						pstmt.setObject(idx, null);
+						insertLog += " | null ";
+					} else if (NumberUtils.isParsable(val)) { // 可以转换为数字
+						pstmt.setObject(idx, val);
+						insertLog += " | " + val;
+					} else {
+						pstmt.setObject(idx, null); // 其他情况，字符串不能转为数字 设置null
+						insertLog += " | null ";
+					}
 				} else {
 					pstmt.setObject(idx, val);
 					insertLog += " | " + val;
@@ -93,8 +112,17 @@ public class InsertDao {
 		return msg;
 	}
 
+	/**
+	 * 
+	 * @param conn
+	 * @param tableName
+	 * @param fields
+	 * @param fieldsValue 元素不能为null
+	 * @return
+	 * @throws Exception
+	 */
 	public static String execInsertBySheetFieldPo(Connection conn, String tableName, List<SheetFieldPo> fields,
-			List<String> fieldsValue) throws Exception {
+			List<List<String>> rowVals) throws Exception {
 		String msg = "";
 		StringBuilder sql = new StringBuilder("insert into " + tableName + " (");
 		StringBuilder values = new StringBuilder("");
@@ -120,31 +148,59 @@ public class InsertDao {
 		pstmt = conn.prepareStatement(insert);
 		String insertLog = insert;
 
-		int idx = 0;
-		for (int i = 0; i < size; i++) {
-			SheetFieldPo fieldpo = fields.get(i);
-			String val = fieldsValue.get(i);
-			idx++;
-			String type = fieldpo.getColumnClassName().get();
-			int javatype = fieldpo.getColumnType().get();
-			String columnTypeName = fieldpo.getColumnTypeName().get();
-			logger.info("javatype = " + javatype + " | " + columnTypeName);
-			if (CommonUtility.isDateAndDateTime(javatype)) {
-				Date dv = StrUtils.StrToDate_L(val);
-				pstmt.setObject(idx, dv);
-				insertLog += " | " + dv;
-			} else if (CommonUtility.isNum(javatype) && StrUtils.isNullOrEmpty(val.trim())) {
-				pstmt.setObject(idx, null);
-			} else {
-				pstmt.setObject(idx, val);
-				insertLog += " | " + val;
+		for (List<String> fieldsValue : rowVals) {
+			int idx = 0;
+			for (int i = 0; i < size; i++) {
+				SheetFieldPo fieldpo = fields.get(i);
+				String val = fieldsValue.get(i);
+				if (val == null) {
+					val = "";
+				}
+				idx++;
+				int javatype = fieldpo.getColumnType().get();
+				String columnTypeName = fieldpo.getColumnTypeName().get();
+				logger.info("javatype = " + javatype + " | " + columnTypeName);
+				// 时间类型判断
+				if (CommonUtility.isDateAndDateTime(javatype)) {
+					// 空字符串 给字段复制null
+					if (StrUtils.isNullOrEmpty(val.trim())) {
+						pstmt.setObject(idx, null);
+					} else {
+						Date dv = StrUtils.StrToDate_L(val);
+						pstmt.setObject(idx, dv);
+						insertLog += " | " + val;
+					}
+
+					// 数字判断
+				} else if (CommonUtility.isNum(javatype)) {
+					val = val.trim();
+					if (StrUtils.isNullOrEmpty(val)) { // 空字符串， 设置null
+						pstmt.setObject(idx, null);
+						insertLog += " | null ";
+					} else if (NumberUtils.isParsable(val)) { // 可以转换为数字
+						pstmt.setObject(idx, val);
+						insertLog += " | " + val;
+					} else {
+						pstmt.setObject(idx, null); // 其他情况，字符串不能转为数字 设置null
+						insertLog += " | null ";
+					}
+
+				} else {
+					pstmt.setObject(idx, val);
+					insertLog += " | " + val;
+				}
+
 			}
-
+			logger.info(insertLog);
+			pstmt.addBatch();
 		}
-		logger.info(insertLog);
-		int count = pstmt.executeUpdate();
+		int[] count = pstmt.executeBatch();
+		int execCountLen = count.length;
+		logger.info("instert = " + execCountLen);
+//		int count = pstmt.executeUpdate();
 
-		msg = "Ok, Insert " + count + " ;\n" + insertLog;
+		msg = "Ok, Insert " + execCountLen + " ;\n" + insertLog;
 		return msg;
 	}
+
 }
