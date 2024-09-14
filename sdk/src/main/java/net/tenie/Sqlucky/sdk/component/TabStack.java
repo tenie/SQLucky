dd803d4b1c9e8827ad966dd06e3a631539e45e34
turenjie <turj1@yonyou.com>
2024-09-14 22:15:26 +0800

    update

---
+++ sdk/src/main/java/net/tenie/Sqlucky/sdk/component/TabStack.java
@@ -0,0 +1,13 @@
+package net.tenie.Sqlucky.sdk.component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * tab的操作栈,
+ */
+public class TabStack {
+   List<TabStackObj> inStack = new ArrayList<>();
+
+
+}
